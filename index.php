493dc6736f419ac456dee19ab644b7993c42db8b
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2021-03-16 12:56:22 +0100

    modifications pour ajouter un résumé

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
   <head>
     <title>PHP Test</title>
   </head>
+    <?php echo "<p>Mistery Man of the Mathematics</p>";
+    ?>
   <body>
-    <?php echo '<p class=="class-css">Escape Game</p>'; ?> 
+    <?php echo "<p class.css = résumé> On a retrouvé de vieux écrits d'un mathématicien datant de l'Antiquité, mais on ignore le nom de celui-ci...Vous êtes archéologue et vous devez aller à la Grande Bibliothèque, afin de résoudre des énigmes pour retrouver le nom de ce mathématicien.</p>"; ?> 
+    <p><input type="submit" value="start"></p>
   </body>
 </html>
