2c3ed78405e009a3752c0a980a68f2937b493940
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-03-23 11:56:26 +0100

    Create coup_valid.py

---
+++ coup_valid.py
@@ -0,0 +1,16 @@
+liste_pions = (["B1","B2","B3","B4","N1","N2","N3","N4"], ["blanc","blanc","blanc","blanc","noir","noir","noir","noir"])
+
+def coup_valide(pion):
+  for pion in liste_pions :
+    i => 3
+    y =< 4
+    if pion = liste_pions([i],[i]):
+
+      
+      return True
+    
+    elif pion = liste_pions([i],[i]):
+      
+      return True
+    
+    else : False
