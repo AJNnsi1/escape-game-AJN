d7bd2762990425f2e419e3ada12363fbf8ea8bb5
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-03-24 15:20:56 +0100

    Update coup_valid.py

--- coup_valid.py
+++ coup_valid.py
@@ -1,16 +1,26 @@
+
+grille_actuelle = [["B1","B2","B3","B4","*","*","N1","N2","N3","N4"]]
+
 liste_pions = (["B1","B2","B3","B4","N1","N2","N3","N4"], ["blanc","blanc","blanc","blanc","noir","noir","noir","noir"])
 
 def coup_valide(pion):
   for pion in liste_pions :
-    i => 3
-    y =< 4
+    i <= 3
+    y >= 4  # faire en sorte que ce soit les 4 en partant de la fin
     if pion = liste_pions([i],[i]):
+      return(pion)
 
-      
-      return True
+#      for pion in grille_actuelle:
+ #       if grille_actuelle[index(pion) + 1] == 
+
+#      return True
     
-    elif pion = liste_pions([i],[i]):
-      
-      return True
-    
-    else : False
+ #   elif pion = liste_pions([y],[y]):
+
+
+
+  #    retun True
+
+   # else : retun False
+
+coup_valide("B4")
