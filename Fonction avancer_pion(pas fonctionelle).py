733796a47e995b91c6ec3261485c240b410f4d7d
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2021-04-01 17:21:46 +0200

    Fonction avancer_pion avec saut fonctionnelle
    La fonction permet de déplacer les pions de 1 en avant ou en arrière si le pion est noir et de sauter par-dessus d'autres pions
    Avec l'aide de Jade.

--- Fonction avancer_pion(pas fonctionelle).py
+++ Fonction avancer_pion(pas fonctionelle).py
@@ -4,59 +4,70 @@ grille = ["B1", "B2", "B3", "B4", "0", "0", "N1", "N2", "N3", "N4"]
 def avancer_pion(grille, index):
 
     for i in range(1, 5):
-        b = "B" + str(i)
-        n = "N" + str(i)
+      b = "B" + str(i)
+      n = "N" + str(i)
 
-        if grille[index] == b:
+      if grille[index] == b:
 
-          for i in range(1,5):
-            b = "B" + str(i)
-            n = "N" + str(i)
+        if grille[index + 1] == "0" :
 
-            if grille[index + 1] == b or n :
+          t = grille[index]
+          grille[index] = grille[index + 1]
+          grille[index + 1] = t
 
-              if grille[index + 2] == "0" :
+          return grille
+       
+        for i in range(1,5):
+          b = "B" + str(i)
+          n = "N" + str(i)
 
-               t = grille[index]
-               grille[index] = grille[index + 2]
-               grille[index + 2] = t
+          if grille[index + 2] == "0" :
 
-               return grille
+           t = grille[index]
+           grille[index] = grille[index + 2]
+           grille[index + 2] = t
 
-              else :
-                return "impossible"
+           return grille
 
-            else :
-              t = grille[index]
-              grille[index] = grille[index + 1]
-              grille[index + 1] = t
+          else :
 
-              return grille
+             return "impossible"
             
-        elif grille[index] == n:
+      elif grille[index] == n:
 
-          for i in range(1,5) :
-            b = "B" + str(i)
-            n = "N" + str(i)
+         if grille[index - 1] == "0" :
 
-            if grille[index - 1] == b or n :
+           t = grille[index]
+           grille[index] = grille[index - 1]
+           grille[index - 1] = t
 
-              if grille[index - 2] == "0" :
+           return grille
 
-               t = grille[index]
-               grille[index] = grille[index - 2]
-               grille[index - 2] = t
+         else :
 
-               return grille
+             for i in range(1,5) :
+               b = "B" + str(i)
+               n = "N" + str(i)
 
-              else :
-                return "impossible"
+               if grille[index - 1] == b or n :
 
-    if grille[index] == "0":
+                  if grille[index - 2] == "0" :
+
+                    t = grille[index]
+                    grille[index] = grille[index - 2]
+                    grille[index - 2] = t
+
+                    return grille
+
+                  else :
+
+                     return "impossible"
+
+      if grille[index] == "0":
 
         return "impossible"
 
 
-nouvelle_grille = avancer_pion(grille, 2)
+nouvelle_grille = avancer_pion(grille, 9)
 
 print(nouvelle_grille)
