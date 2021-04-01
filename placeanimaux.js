674cf8c984165c9068df2f6b7864fa1ee2393d1f
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-04-01 17:57:47 +0200

    Create placeanimaux.js

---
+++ placeanimaux.js
@@ -0,0 +1,16 @@
+
+function place_chat(id) {
+  document.getElementById("vert").innerHTML = "place 1";
+  document.getElementById("vert").style.color = "green";
+}
+
+
+function place_cochon(id) {
+  document.getElementById("rose").innerHTML = "place 4";
+  document.getElementById("rose").style.color = "pink";
+}
+
+function place_vache(id) {
+  document.getElementById("rouge").innerHTML = "place 2";
+  document.getElementById("rouge").style.color = "red";
+}
