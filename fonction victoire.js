6cfe7fd2b75dea4d987afb804f6d6f3306106f62
aojeda5 <80753563+aojeda5@users.noreply.github.com>
2021-04-01 17:42:43 +0200

    Update fonction victoire.js
    Et voici la fonction victoire traduite en javascript :)

--- fonction victoire.js
+++ fonction victoire.js
@@ -1,20 +1,5 @@
 <script language="JavaScript">
 
-var nl = getNewLine()
-
-function getNewLine() {
-	var agent = navigator.userAgent
-
-	if (agent.indexOf("Win") >= 0)
-		return "\r\n"
-	else
-		if (agent.indexOf("Mac") >= 0)
-			return "\r"
-
- 	return "\r"
-
-}
-
 pagecode = 'index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 pions_noirs=["N1", "N2", "N3", "N4"]
 pions_blancs=["B1", "B2", "B3", "B4"]
