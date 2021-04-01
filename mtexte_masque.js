edb07a45be32441e5fb9d4a106fb7f39730d9b29
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-04-01 17:57:28 +0200

    Create mtexte_masque.js
    il faut aller voir placeanimaux.js

---
+++ mtexte_masque.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+  <script type="text/javascript" src="placeanimaux.js"></script
+
+<body>
+
+  <p>il y a</p><p id="vert" onclick="place_chat()">un chat.</p>
+  <p>Puis est entré</p>
+  <p id="rose" onclick="place_cochon()">un cochon.</p> 
+  <p>Seulement, </p>
+  <p id="rouge" onclick="place_vache()">la femme du boeuf</p>
+  <p> n avait plus de place.</p>
+
+</body>
+</html>
