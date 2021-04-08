a0daa4ae233bee9ce61092688016ec294cc283ca
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2021-04-08 18:30:01 +0200

    Début de la traduction de la fonction avancer_pion avec saut.py
    Je ne comprends pourquoi l'appel de fonction ne fonctionne pas.

---
+++ Avancer_pion avec saut.js
@@ -0,0 +1,25 @@
+var grille=new Array ();
+grille[0]="B1";
+grille[1]="B2";
+grille[2]="B3";
+grille[3]="B4";
+grille[4]="0";
+grille[5]="0";
+grille[6]="N1";
+grille[7]="N2";
+grille[8]="N3";
+grille[9]="N4";
+
+Function avancer_pion(grille,index) {
+  for (let i = 0;i = 4;i++){
+    let b = "B" + string(i)
+    let n = "N" + string(i)
+
+
+
+
+  }
+
+
+
+}
