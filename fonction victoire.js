68dbfe8ec663ec880b3372c73f4ffd0a9575bad0
aojeda5 <80753563+aojeda5@users.noreply.github.com>
2021-05-04 12:01:49 +0200

    Update fonction victoire.js

--- fonction victoire.js
+++ fonction victoire.js
@@ -1,17 +1,25 @@
 <script language="JavaScript">
-
-pagecode = 'index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+gv=["B4","B3"....]
+ gc=["B4","B3"....]
+#pagecode = 'index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 pions_noirs=["N1", "N2", "N3", "N4"]
 pions_blancs=["B1", "B2", "B3", "B4"]
 <button onclick="this.innerHTML = valider()"> Valider</button>
 
-def victoire(index, pions_blancs, pions_noirs):
-  if index[0,1,2,3]==pions_noirs[0,1,2,3] and index[6,7,8,9]==pions_blancs[0,1,2,3]:
+function victoire(grille_c)
+{
+  victoire=false;
+  
+  return victoire;
+}
+
+"""
+  if index[0,1,2,3]==pions_noirs[6,7,8,9] and index[6,7,8,9]==pions_blancs[0,1,2,3]:
     print("Félicitations! Vous avez réussi cette énigme! On passe à la suivante? ☺")
   else:
       print("Réessayez encore! Courage, vous allez y arriver! ☺")
 
 if "button" is clicked:
   return victoire'
-
+"""
 </script>
