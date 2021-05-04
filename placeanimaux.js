a446d2129b39a75565fac3667667ed3bc4c2bc45
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-05-04 12:23:11 +0200

    Update placeanimaux.js

--- placeanimaux.js
+++ placeanimaux.js
@@ -1,16 +1,15 @@
-
 function place_chat(id) {
-  document.getElementById("vert").innerHTML = "place 1";
+  document.getElementById("vert").innerHTML = "CHAT en place 2";
   document.getElementById("vert").style.color = "green";
 }
 
 
 function place_cochon(id) {
-  document.getElementById("rose").innerHTML = "place 4";
+  document.getElementById("rose").innerHTML = "COCHON en place 1";
   document.getElementById("rose").style.color = "pink";
 }
 
 function place_vache(id) {
-  document.getElementById("rouge").innerHTML = "place 2";
+  document.getElementById("rouge").innerHTML = "VACHE en place 4";
   document.getElementById("rouge").style.color = "red";
 }
