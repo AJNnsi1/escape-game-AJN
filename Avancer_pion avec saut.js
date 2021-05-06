cc26512c5d8b5fab8ce5fe35be0c56732bf8d969
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2021-05-06 17:53:34 +0200

    Progression de la traduction de la fonction avancer pion avec saut.py en JS
    Plusieurs problèmes restants:
    - impossibilité de "print" avec du Java Script
    - ( Unexpected token '[' )problème avec un crochet de "grille_courante[index]"    (ligne : 24)

--- Avancer_pion avec saut.js
+++ Avancer_pion avec saut.js
@@ -10,14 +10,14 @@ grille_courante[7]="N2";
 grille_courante[8]="N3";
 grille_courante[9]="N4";
 
-Function av(grille_courante,index) {
+function avancer_pion(grille_courante,index) {
   for (let i = 0;i = 4;i++){
     let b = "B" + string(i)
     let n = "N" + string(i)
 
-    if grille_courante[index] == b {
+    if (grille_courante[index] == b) {
 
-      if grille_courante[index + 1] == "0" {
+      if (grille_courante[index + 1] == "0") {
         
         let t = grille_courante[index]
         let grille_courante[index] = grille_courante[index + 1]
@@ -30,7 +30,7 @@ Function av(grille_courante,index) {
         let b = "B" + string(i)
         let n = "N" + string(i)
 
-        if grille_courante[index + 2] == "0" {
+        if (grille_courante[index + 2] == "0") {
 
         let t = grille_courante[index]
         let grille_courante[index] = grille_courante[index + 2]
@@ -42,9 +42,9 @@ Function av(grille_courante,index) {
           return "impossible"
         }
       } 
-    } else if grille_courante[index] == n {
+    } else if (grille_courante[index] == n) {
 
-      if grille_courante[index - 1] == "0" {
+      if (grille_courante[index - 1] == "0") {
         
         let t = grille_courante[index]
         let grille_courante[index] = grille_courante[index - 1]
@@ -55,15 +55,32 @@ Function av(grille_courante,index) {
             for (let i = 0;i = 4;i++){
             let b = "B" + string(i)
             let n = "N" + string(i)
-              if 
 
+              if (grille_courante[index - 1] = b / n) {
+                if (grille_courante[index - 2] = "0"){
 
+                  let t = grille_courante[index]
+                  let grille_courante[index] = grille_courante[index + 2]
+                  let grille_courante[index + 2] = t
+
+                  return grille_courante
+                  } else { 
+                    
+                    return "impossible"
+                     }
+              }
          }
         }
-
-
   }
-
-
-
+if (grille_courante[index] = "0"){
+  return "impossible"
 }
+}
+
+
+
+let nouvelle_grille = av(grille_courante,1)
+
+//var grille_courante = ["B1","B2","B3","B4","0","0","N1","N2","N3","N4"]
+//console.log(grille_courante[0]);
+
