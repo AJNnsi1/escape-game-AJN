63da32d999a80cde3042c3eb533f527cd9a41666
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-05-11 12:55:05 +0200

    en chantier
    début du codage de la fonction interversion de deux image - ici j'essaye de voir si ça marche avec des id définis, mais après j'aimerais faire en sorte que quand la deuxième image est sélectionner cela pourras être n'importe quel image. - problèmes : intervertir des image dans un tableau - replit n'a pas voulu afficher ce que je modifier alors j'ai dû faire a "l'aveugle"

--- ANIMAUX.js
+++ ANIMAUX.js
@@ -1,3 +1,4 @@
+
 function place_chat(id) {
   document.getElementById("vert").innerHTML = "CHAT en place 2";
   document.getElementById("vert").style.color = "green";
@@ -12,4 +13,28 @@ function place_cochon(id) {
 function place_vache(id) {
   document.getElementById("rouge").innerHTML = "VACHE en place 4";
   document.getElementById("rouge").style.color = "red";
+}
+
+// ces 3 fonctions sont l'aide pour ranger le tableau - quand on click dessus ça change de couleur et de mots
+
+object.onclick = function(){deplacement_imagelapin};
+
+function deplacement_imagelapin(id) {
+  document.getElementsById("lapin").;
+  function change_image(id) {
+
+  if (document.getElementById("lapin");
+    {
+      document.getElementById("chat");
+      
+      
+      } // on iverce les deux image selectionnées, ici lapin et chat
+
+    else 
+      {
+        document.getElementById("lapin");
+        } // si aucune autre image n'est selectionnées alors rien ne se passe
+  }
+
+
 }
