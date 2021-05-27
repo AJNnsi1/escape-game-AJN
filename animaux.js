a63433de07d96efd83e81045870e7b608cdd0d50
N0m111 <80753701+N0m111@users.noreply.github.com>
2021-05-27 16:27:10 +0200

    Update and rename ANIMAUX.js to animaux.js

---
+++ animaux.js
@@ -0,0 +1,14 @@
+function place_chat(id) {
+  document.getElementById("chat").innerHTML = "chat en place 2";
+  document.getElementById("chat").style.color = "red";
+}
+
+function place_cochon(id) {
+  document.getElementById("cochon").innerHTML = "cochon en place 1";
+  document.getElementById("cochon").style.color = "red";
+}
+
+function place_vache(id) {
+  document.getElementById("vache").innerHTML = "vache en place 4";
+  document.getElementById("vache").style.color = "red";
+}
